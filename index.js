896f6d1d80067d34139c0c499836727e3aec5ee2
vivekkashyap <vivek.kashyap102@gmail.com>
2022-08-18 14:55:32 +0530

    Implemeted register API and added cors

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 require('./database/config');
 const User = require('./database/Users');
 const app = express();
+const cors = require('cors');
 
-app.post("/register", (req, res) => {
-    res.send("API in progress...");
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
 })
 
 app.get('/', (req, res)=> {
