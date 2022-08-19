e1ee3f7e0921d01417f5247aaba048cdc8624bea
vivekkashyap <vivek.kashyap102@gmail.com>
2022-08-19 12:22:29 +0530

    Surrounded with try catch block

--- index.js
+++ index.js
@@ -9,41 +9,57 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, res) => {
-    let user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password;
-    delete result.__v;
-    res.send(result);
+    try {
+        let user = new User(req.body);
+        let result = await user.save();
+        result = result.toObject();
+        delete result.password;
+        delete result.__v;
+        res.send(result);
+    } catch (err) {
+        console.error(err);
+    }
 })
 
 app.post("/login", async (req, res) => {
-    let user = await User.findOne(req.body).select("-password").select("-__v");
-    if (req.body.password && req.body.email && user) {
-        res.send(user);
-    } else {
-        res.send({ result: 'No user Found' });
+    try {
+        let user = await User.findOne(req.body).select("-password").select("-__v");
+        if (req.body.password && req.body.email && user) {
+            res.send(user);
+        } else {
+            res.send({ result: 'No user Found' });
+        }
+    } catch (err) {
+        console.error(err);
     }
 });
 
 app.post("/add-product", async (req, res) => {
-    let product = new Product(req.body);
-    let result = await product.save();
-    result = result.toObject();
-    delete result.__v;
-    res.send(result);
-});
-
-app.get("/products", async (req, res) => {
-    let products = await Product.find();
-    if (products.length > 0) {
-        res.send(products);
-    } else {
-        res.send({ result: 'No products found' });
+    try {
+        let product = new Product(req.body);
+        let result = await product.save();
+        result = result.toObject();
+        delete result.__v;
+        res.send(result);
+    } catch (err) {
+        console.error(err);
     }
 });
 
-app.get('/', (req, res)=> {
+app.get("/products", async (req, res) => {
+    try {
+        let products = await Product.find();
+        if (products.length > 0) {
+            res.send(products);
+        } else {
+            res.send({ result: 'No products found' });
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.get('/', (req, res) => {
     console.log("App is Working...")
 });
 
